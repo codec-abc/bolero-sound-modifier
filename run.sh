2299d47caf2cf94b6628adcb6befad004f4e623a
codec-abc <viot.camille@outlook.com>
2020-11-15 18:16:40 +0100

    up

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 dotnet run -p SoundServer/src/SoundServer.Server/
 dotnet build SoundServer/src/SoundServer.Client/
+
+dotnet publish SoundServer/SoundServer.sln -c Release -r linux-arm64
+# then copy ./SoundServer/src/SoundServer.Server/bin/Release/netcoreapp5.0/linux-arm64/publish/ on target
+# then go into publish dir and type chmod +x SoundServer.Server
+# then launch server with ./SoundServer.Server
